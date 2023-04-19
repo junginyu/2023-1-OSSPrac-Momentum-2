a48c3d0ae584341c333bd3dd901470d546a4c55a
Thina <frog7134@gmail.com>
2023-04-19 18:42:01 +0900

    getSubstract 함수 추가

--- SumFunc.py
+++ SumFunc.py
@@ -1,4 +1,9 @@
 def getSum(a):
     return int(a*(a+2)/2)
-n = int(input("정수를 입력하세요 : "))
+n = int(input("더할 정수를 입력하세요 : "))
 print(getSum(n))
+
+def getSubstract(a):
+    return int(a*(a-2)/2)
+m = int(input("뺄 정수를 입력하세요 : "))
+print(getSubstract(n))
